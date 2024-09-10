02907f028d3f65341318bdf3bb4ec4aadfa7369f
Jareth Baur <jarethbaur0223@gmail.com>
2024-09-10 19:22:31 +0800

    Added contents to dashboard

--- src/org/jareth/DashboardController.java
+++ src/org/jareth/DashboardController.java
@@ -6,7 +6,11 @@ package org.jareth;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -21,6 +25,22 @@ public class DashboardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void btnPressMeActionPerformed(MouseEvent event) {
+        // Create a new Alert of type INFORMATION
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Congrats!");
+        alert.setHeaderText(null);
+        alert.setContentText("You pressed the button!");
+
+        // Show the alert and wait for the user to close it
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void btnPressMe(ActionEvent event) {
+    }
+
 }
